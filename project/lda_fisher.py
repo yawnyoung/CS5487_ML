2501b3913779a6c974da8212ea489c916a2ddf32
yawnyoung <yawnyoung@gmail.com>
2018-11-30 12:32:15 +0800

    finish fisher

--- project/lda_fisher.py
+++ project/lda_fisher.py
@@ -6,12 +6,13 @@ author: Yajue Yang
 
 from project.data_processing import load_train_test_data
 import numpy as np
+import math
 
 # Number of classes
 NUM_CLASSES = 3
 
 
-def fisher_discriminant(x, y):
+def fisher_discriminant_features(x, y):
 
     num_data = len(x)
     dim_feat = len(x[0])
@@ -42,27 +43,52 @@ def fisher_discriminant(x, y):
     eig_val = eig_val[idx]
     eig_vec = eig_vec[:, idx]
 
-    Theta = eig_vec[0][:, np.newaxis]
-    Theta = np.append(Theta, eig_vec[1][:, np.newaxis], axis=1)
+    Theta = (eig_vec[0] / np.linalg.norm(eig_vec[0]))[:, np.newaxis]
+    Theta = np.append(Theta, (eig_vec[1] / np.linalg.norm(eig_vec[1]))[:, np.newaxis], axis=1)
 
-    print(Theta.transpose() @ x_classified[0])
-    print(Theta.transpose() @ x_classified[1])
-    print(Theta.transpose() @ x_classified[2])
+    print(Theta.shape)
 
-    # print(eig_vec)
+    return Theta, x_mean
 
 
+def euclidean_dist(x, y):
+    return np.linalg.norm((x - y))
 
 
+def classify(dist_func, x, df_param, class_centroid):
+
+    min_dist = math.inf
+    min_idx = 0
+
+    for i in range(3):
+        x_df = df_param.transpose() @ x
+        cc_df = df_param.transpose() @ class_centroid[i]
+
+        dist = dist_func(x_df, cc_df)
+
+        if dist < min_dist:
+            min_dist = dist
+            min_idx = i+1
+
+    return min_idx, min_dist
 
 
+def test_stat(dist_func, test_data, df_param, class_centroids):
 
+    x = test_data[1]
+    y_true = test_data[0]
+    num_test = len(x)
 
+    for i in range(num_test):
+        c_idx, dist = classify(dist_func, np.array(x[i]), df_param, class_centroids)
 
+        print(c_idx, y_true[i])
 
 
 if __name__ == '__main__':
 
     train_data, test_data = load_train_test_data()
 
-    fisher_discriminant(train_data[1], train_data[0])
+    df_param, c_centroids = fisher_discriminant_features(train_data[1], train_data[0])
+
+    test_stat(euclidean_dist, test_data, df_param, c_centroids)
