5ab27291e609dbd3e37c6ffddbea78b45fae8d21
yawnyoung <yawnyoung@gmail.com>
2018-12-03 22:28:11 +0800

    finish PA_2 part1

--- PA_2/clustering_alg.py
+++ PA_2/clustering_alg.py
@@ -5,6 +5,8 @@ author: Yajue
 """
 
 import numpy as np
+from scipy.stats import multivariate_normal
+import math
 
 
 def one_true(conds):
@@ -63,6 +65,13 @@ def update_kmeans_mean(x, z):
 
 
 def k_means(x, init_mean, epsilon):
+    """
+    K-Means clustering algorithm
+    :param x: data
+    :param init_mean: initial means
+    :param epsilon: error threshold
+    :return: means and labels of each data
+    """
 
     num_class = init_mean.shape[1]
     num_data = x.shape[1]
@@ -85,6 +94,172 @@ def k_means(x, init_mean, epsilon):
     return curr_mean, z
 
 
+def update_gmm_z(x, curr_pis, curr_means, curr_cov):
+
+    num_class = curr_pis.size
+    num_data = x.shape[1]
+
+    z = np.zeros((num_class, num_data))
+
+    for i in range(num_data):
+
+        for k in range(num_class):
+
+            z_numerator = curr_pis[k] * multivariate_normal.pdf(x[:, i],
+                                                                mean=curr_means[:, k],
+                                                                cov=curr_cov[:, :, k])
+
+            z_den = 0
+
+            for j in range(num_class):
+                z_den += curr_pis[j] * multivariate_normal.pdf(x[:, i], mean=curr_means[:, j], cov=curr_cov[:, :, j])
+
+            # print('num: {}, den: {}'.format(z_numerator, z_den))
+
+            z[k, i] = z_numerator / z_den
+
+    return z
 
 
+def update_gmm_params(x, z):
+
+    num_class = z.shape[0]
+    num_data = x.shape[1]
+    dim_feat = x.shape[0]
+
+    count_class = np.array([np.sum(z[k, :]) for k in range(num_class)])
+
+    new_pis = count_class / num_data
+
+    new_means = np.zeros((dim_feat, num_class))
+    new_covs = np.zeros((dim_feat, dim_feat, num_class))
+    for k in range(num_class):
+
+        for i in range(num_data):
+            new_means[:, k] += z[k, i] * x[:, i]
+
+        new_means[:, k] /= count_class[k]
+
+        for i in range(num_data):
+            mean_diff = (x[:, i] - new_means[:, k])[:, np.newaxis]
+            new_covs[:, :, k] += np.diag(np.diag(z[k, i] * (mean_diff @ np.transpose(mean_diff))))
+
+        # print(new_means[:, k])
+        new_covs[:, :, k] /= count_class[k]
+
+    return new_pis, new_means, new_covs
+
+
+def gmm_log_likelihood(x, curr_pis, curr_means, curr_covs):
+
+    num_class = curr_means.shape[1]
+    num_data = x.shape[1]
+
+    log_likelihood = 0
+
+    for i in range(num_data):
+
+        likelihood = 0
+
+        for k in range(num_class):
+
+            likelihood += curr_pis[k] * multivariate_normal.pdf(x[:, i], curr_means[:, k], curr_covs[:, :, k])
+
+        log_likelihood += np.log(likelihood)
+
+    return log_likelihood
+
+
+def EM_GMM(x, init_pis, init_means, init_cov, inc_thld):
+
+    curr_pis = np.copy(init_pis)
+    curr_means = np.copy(init_means)
+    curr_cov = np.copy(init_cov)
+
+    q = gmm_log_likelihood(x, curr_pis, curr_means, curr_cov)
+
+    q_inc = math.inf
+
+    num_class = init_pis.size
+    num_data = x.shape[1]
+
+    z = np.zeros((num_class, num_data))
+
+    while q_inc > inc_thld:
+
+        z = update_gmm_z(x, curr_pis, curr_means, curr_cov)
+        curr_pis, curr_means, curr_cov = update_gmm_params(x, z)
+
+        q_new = gmm_log_likelihood(x, curr_pis, curr_means, curr_cov)
+
+        q_inc = q_new - q
+
+        q = q_new
+
+        print('q: {}, q_inc: {}'.format(q, q_inc))
+
+    print(curr_pis)
+    print(curr_means)
+
+    return curr_pis, curr_means, curr_cov, z
+
+
+def mean_shift(x, h, x_init, ct):
+
+    num_data = x.shape[1]
+    dim_param = x.shape[0]
+
+    x_err = math.inf
+
+    x_curr = np.copy(x_init)
+    print('before updating: ', x_curr)
+
+    while x_err > ct:
+
+        x_expectation = np.zeros(dim_param)
+
+        for i in range(num_data):
+            x_expectation += x[:, i] * multivariate_normal.pdf(x[:, i], x_curr, h * h * np.identity(dim_param))
+
+        x_next = x_expectation / np.sum([multivariate_normal.pdf(x[:, i], x_curr, h * h * np.identity(dim_param))
+                                         for i in range(num_data)])
+
+        x_err = np.linalg.norm(x_curr - x_next)
+
+        x_curr = x_next
+
+    print('after updating: ', x_curr)
+
+    return x_curr
+
+
+def mean_shift_clustering(x, h, ct, pr_min):
+
+    num_data = x.shape[1]
+
+    x_cnvg = np.ndarray(x.shape)
+
+    peaks = []
+    z = np.ndarray(num_data)
+
+    for i in range(num_data):
+        print(i)
+
+        x_cnvg[:, i] = mean_shift(x, h, x[:, i], ct)
+
+        if len(peaks) == 0:
+            peaks.append(x_cnvg[:, i])
+            z[i] = 0
+
+        else:
+            dists = np.array([np.linalg.norm(p_v - x_cnvg[:, i]) for p_v in peaks])
+            if np.min(dists) < pr_min:
+                z[i] = np.argmin(dists)
+            else:
+                peaks.append(x_cnvg[:, i])
+                z[i] = len(peaks) - 1
+
+    # print(x_cnvg)
+
+    return x_cnvg, peaks, z
 
--- PA_2/part1_soln.py
+++ PA_2/part1_soln.py
@@ -5,15 +5,146 @@ author: Yajue Yang
 """
 
 from PA_2.data_processing import *
-from PA_2.clustering_alg import k_means
+from PA_2.clustering_alg import k_means, EM_GMM, mean_shift_clustering
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import matplotlib.cm as cm
+
+POINT_COLOR = ['r', 'g', 'b', 'y']
 
 
-if __name__ == '__main__':
-    x, y = load_synthetic_data('A')
+def plot_k_means_clustering(x, z, means, fig_name):
 
-    init_mean = np.array([[-3, 3, 4, -2], [3, 4, -3, -2]])
+    num_data = x.shape[1]
+
+    for i in range(num_data):
+
+        c_idx = np.argmax(z[:, i])
+
+        plt.plot(x[0, i], x[1, i], '.', color=POINT_COLOR[c_idx])
+
+    plt.plot(means[0, :], means[1, :], 'ko')
+    plt.title(fig_name)
+    plt.xlabel('$x_1$')
+    plt.ylabel('$x_2$')
+
+    plt.show()
+
+
+def plot_cov_ellipse(cov, pos, nstd=2, ax=None, **kwargs):
+
+    def eigsorted(cov):
+        vals, vecs = np.linalg.eigh(cov)
+        order = vals.argsort()[::-1]
+        return vals[order], vecs[:,order]
+
+    if ax is None:
+        ax = plt.gca()
+
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+
+    # Width and height are "full" widths, not radius
+    width, height = 2 * nstd * np.sqrt(vals)
+    ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, fill=False, **kwargs)
+
+    ax.add_artist(ellip)
+    return ellip
+
+
+def plot_gmm_clustering(x, means, cov, z, fig_name):
+
+    num_data = x.shape[1]
+
+    for i in range(num_data):
+
+        c_idx = np.argmax(z[:, i])
+
+        plt.plot(x[0, i], x[1, i], '.', color=POINT_COLOR[c_idx])
+
+    plt.plot(means[0, :], means[1, :], 'ko')
+    plt.title(fig_name)
+    plt.xlabel('$x_1$')
+    plt.ylabel('$x_2$')
+
+    for i in range(4):
+        plot_cov_ellipse(cov[:, :, i], means[:, i])
+
+    plt.show()
+
+
+def test_k_means():
+    x, y = load_synthetic_data('C')
+
+    # init_mean = np.array([[-3, 3, 4, -2], [3, 4, -3, -2]])
+    # init_mean = np.random.rand(2, 4) * 10
+    init_mean = np.random.uniform(-15, 15, 2 * 4).reshape((2, 4))
+    print(init_mean)
+
     epsilon = np.array([0.001, 0.001, 0.001, 0.001])
 
     curr_mean, z = k_means(x, init_mean, epsilon)
 
-    print(curr_mean)
+    plot_k_means_clustering(x, z, curr_mean, 'dataC',)
+
+    # print(curr_mean)
+    # print(z.shape)
+
+
+def test_em_gmm():
+
+    data_name = 'C'
+
+    x, y = load_synthetic_data(data_name)
+
+    dim_param = x.shape[0]
+    init_pis = np.array([0.25, 0.25, 0.25, 0.25])
+    # init_mean = np.array([[-3, 3, 4, -2], [3, 4, -3, -2]])
+    init_mean = np.random.uniform(-15, 15, 2 * 4).reshape((2, 4))
+
+    # epsilon = np.array([0.001, 0.001, 0.001, 0.001])
+    #
+    # init_mean, z = k_means(x, init_mean, epsilon)
+
+    print(init_mean)
+    init_cov = np.ndarray((dim_param, dim_param, 4))
+    for i in range(4):
+        init_cov[:, :, i] = np.identity(dim_param) * 0.1
+
+    pis, means, cov, z = EM_GMM(x, init_pis, init_mean, init_cov, 0.001)
+
+    plot_gmm_clustering(x, means, cov, z, 'data' + data_name)
+
+
+def test_mean_shift():
+
+    data_name = 'A'
+
+    x, y = load_synthetic_data(data_name)
+
+    h = 0.5
+    ct = 0.001
+    pr_min = 0.1
+    x_cnvg, peaks, z = mean_shift_clustering(x, h, ct, pr_min)
+
+    colors = cm.rainbow(np.linspace(0, 1, len(peaks)))
+    num_data = x.shape[1]
+    for i in range(num_data):
+        plt.plot(x[0, i], x[1, i], '.', color=colors[int(z[i])])
+
+    plt.title('data' + data_name)
+    plt.xlabel('$x_1$')
+    plt.ylabel('$x_2$')
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    # test_k_means()
+
+    # test_em_gmm()
+    test_mean_shift()
+
+    # colors = cm.rainbow(np.linspace(0, 1, 10))
+    #
+    # plt.plot(1, 2, '.', )
